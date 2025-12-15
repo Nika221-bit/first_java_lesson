bca6c68da1894fde16341ed4bbbe23f264855dc0
NikolozLekishvili <K0K0L0z1922@gmail.com>
2025-12-15 06:27:06 +0400

    1

---
+++ app.js
@@ -0,0 +1,38 @@
+// გამარჯობა
+let greeting = "გამარჯობა";
+console.log(greeting);
+alert(greeting);
+
+// დავალება 1.1
+let counter = 22;
+counter ++;
+console.log(counter);
+alert(counter)
+// დავალება 1.2
+let num = 5;
+alert(num);
+num = 10;
+// alert(num)-ი რომ ეწეროს num=10-ის ქვემოთ, მაშინ alert-ში 10 გამოიტანს
+// დავალება 1.3
+let b = 10 + 20 + 30;
+alert(b);
+// დავალება 1.4
+let a = 100;
+b = 40;
+let c = a - b;
+let d = 20;
+let result = c + d;
+alert(result);
+// დავალება 1.5
+13
+// დავალება 1.6
+a = 13; 
+b = 5; 
+c = a % b;
+console.log(c);
+alert(c);// 3
+// დავალება 1.7
+72
+// დავალება 1.8
+const birthday = "22.05.2009";
+console.log(birthday);
